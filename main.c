6367fc4743fc3002dd3245242c88ad1900b147d1
Sven Andersson <sweetfish@gmail.com>
2025-03-08 22:59:11 +0100

    third

--- main.c
+++ main.c
@@ -14,10 +14,20 @@
 #define WINDOW_HEIGHT 200
 #define BUTTON_WIDTH 150
 #define BUTTON_HEIGHT 30
+#define RADIO_WIDTH 90
+
+#define ID_TIFF_RADIO 2
+#define ID_JPG_RADIO 3
+#define ID_PNG_RADIO 4
+#define ID_DEMOSAIC_CHECK 5
+
+#define FIT_CONVERTER_NAME L"Stupid Simple FIT Converter"
+#define FIT_CONVERTER_VERSION L"0.1.0"
 
 // Global variables
-HWND hwndButton, hwndStatus;
+HWND hwndButton, hwndStatus, hwndTiffRadio, hwndJpgRadio,hwndPngRadio, hwndDemosaicCheck;
 HINSTANCE hInstance;
+
 
 // FITS header card structure (80 bytes each)
 #pragma pack(push, 1)
@@ -51,7 +61,7 @@ int parse_card_value(const char* card, const char* keyword, int* value) {
 // Function declarations
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void HandleConversion(HWND hwnd);
-int ConvertFITtoTIF(const wchar_t* inputPath);
+int ConvertFITtoTIF(const wchar_t* inputPath, BOOL useTiff, BOOL demosaic);
 void ShowError(HWND hwnd, const wchar_t* format, ...);
 void UpdateStatus(const wchar_t* message, BOOL isError);
 
@@ -76,7 +86,7 @@ int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PWSTR pCmdLine, in
     HWND hwnd = CreateWindowExW(
         0,                              // Optional window styles
         CLASS_NAME,                     // Window class
-        L"FIT to TIF Converter",        // Window text
+        FIT_CONVERTER_NAME " " FIT_CONVERTER_VERSION,        // Window text
         WS_OVERLAPPEDWINDOW & ~(WS_MAXIMIZEBOX | WS_THICKFRAME), // Window style
         CW_USEDEFAULT, CW_USEDEFAULT,   // Position
         WINDOW_WIDTH, WINDOW_HEIGHT,     // Size
@@ -106,13 +116,28 @@ int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PWSTR pCmdLine, in
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_CREATE: {
-            // Create "Select FIT File" button
+            // Add Demosaic checkbox at the top
+            hwndDemosaicCheck = CreateWindowW(
+                L"BUTTON",
+                L"Demosaic RGGB",
+                WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,
+                (WINDOW_WIDTH - BUTTON_WIDTH) / 2,
+                10,  // At the top
+                BUTTON_WIDTH,
+                20,
+                hwnd,
+                (HMENU)ID_DEMOSAIC_CHECK,
+                hInstance,
+                NULL
+            );
+
+            // Create "Select FIT File" button (moved down slightly)
             hwndButton = CreateWindowW(
                 L"BUTTON",
                 L"Select FIT File",
                 WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
                 (WINDOW_WIDTH - BUTTON_WIDTH) / 2,
-                50,
+                80,
                 BUTTON_WIDTH,
                 BUTTON_HEIGHT,
                 hwnd,
@@ -121,13 +146,56 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 NULL
             );
 
-            // Create status text
+            // Create radio buttons for output format (moved down slightly)
+            hwndTiffRadio = CreateWindowW(
+                L"BUTTON",
+                L"TIFF",
+                WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON | WS_GROUP,
+                (WINDOW_WIDTH / 2 - RADIO_WIDTH * 1.5),
+                40,
+                RADIO_WIDTH,
+                20,
+                hwnd,
+                (HMENU)ID_TIFF_RADIO,
+                hInstance,
+                NULL
+            );
+
+            hwndJpgRadio = CreateWindowW(
+                L"BUTTON",
+                L"JPG (8bit)",
+                WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                (WINDOW_WIDTH / 2 - RADIO_WIDTH * 0.5),
+                40,
+                RADIO_WIDTH,
+                20,
+                hwnd,
+                (HMENU)ID_JPG_RADIO,
+                hInstance,
+                NULL
+            );
+
+            hwndPngRadio = CreateWindowW(
+                L"BUTTON",
+                L"PNG (8bit)",
+                WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+                (WINDOW_WIDTH / 2 + RADIO_WIDTH * 0.5),
+                40,
+                RADIO_WIDTH,
+                20,
+                hwnd,
+                (HMENU)ID_PNG_RADIO,
+                hInstance,
+                NULL
+            );
+
+            // Create status text (moved down slightly)
             hwndStatus = CreateWindowW(
                 L"STATIC",
-                L"Select a .FIT file to convert to .TIF format",
+                L"Select a .FIT file to convert",
                 WS_VISIBLE | WS_CHILD | SS_CENTER,
                 10,
-                100,
+                120,
                 WINDOW_WIDTH - 20,
                 50,
                 hwnd,
@@ -135,11 +203,16 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 hInstance,
                 NULL
             );
+
+            // Set defaults
+            SendMessage(hwndTiffRadio, BM_SETCHECK, BST_CHECKED, 0);
+            SendMessage(hwndDemosaicCheck, BM_SETCHECK, BST_CHECKED, 0);
             return 0;
         }
 
         case WM_COMMAND: {
             if (LOWORD(wParam) == 1) {  // Button clicked
+                UpdateStatus(L"Crunching data...", 0);
                 HandleConversion(hwnd);
             }
             return 0;
@@ -178,7 +251,10 @@ void HandleConversion(HWND hwnd) {
     filename[0] = '\0';
 
     if (GetOpenFileNameW(&ofn)) {
-        if (ConvertFITtoTIF(filename)) {
+        BOOL useTiff = (SendMessage(hwndTiffRadio, BM_GETCHECK, 0, 0) == BST_CHECKED);
+        BOOL useJpg = (SendMessage(hwndJpgRadio, BM_GETCHECK, 0, 0) == BST_CHECKED);
+        BOOL demosaic = (SendMessage(hwndDemosaicCheck, BM_GETCHECK, 0, 0) == BST_CHECKED);
+        if (ConvertFITtoTIF(filename, useTiff ? 0 : useJpg ? 1 : 2, demosaic)) {
             UpdateStatus(L"Conversion successful!", FALSE);
         }
     }
@@ -202,7 +278,83 @@ uint8_t write_simple_tiff(const char *filepath, void *image_data, int bitpix, si
     return 1;
 }
 
-int ConvertFITtoTIF(const wchar_t* inputPath) {
+void demosaic_RGGB_16bit(uint16_t *raw_data, uint16_t *output_image, int width, int height) {
+    int i, j;
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            int pixel_index = i * width*3 + j*3;
+            int r, g, b;
+
+            if (i % 2 == 0 && j % 2 == 0) {
+                // Red pixel
+                r = raw_data[pixel_index];
+                g = (raw_data[pixel_index + 1] + raw_data[pixel_index + width*3]) / 2; // average of neighboring green pixels
+                b = (raw_data[pixel_index + width*3 + 1] + raw_data[pixel_index + width*3 - 1]) / 2; // average of neighboring blue pixels
+            } else if (i % 2 == 0 && j % 2 == 1) {
+                // Green pixel (even row, odd column)
+                g = raw_data[pixel_index];
+                r = (raw_data[pixel_index - 1] + raw_data[pixel_index + 1]) / 2; // average of neighboring red pixels
+                b = (raw_data[pixel_index + width*3] + raw_data[pixel_index + width*3 - 1]) / 2; // average of neighboring blue pixels
+            } else if (i % 2 == 1 && j % 2 == 0) {
+                // Green pixel (odd row, even column)
+                g = raw_data[pixel_index];
+                r = (raw_data[pixel_index - width*3] + raw_data[pixel_index + width*3]) / 2; // average of neighboring red pixels
+                b = (raw_data[pixel_index - 1] + raw_data[pixel_index + 1]) / 2; // average of neighboring blue pixels
+            } else {
+                // Blue pixel
+                b = raw_data[pixel_index];
+                g = (raw_data[pixel_index - width*3] + raw_data[pixel_index + width*3]) / 2; // average of neighboring green pixels
+                r = (raw_data[pixel_index - width*3 - 1] + raw_data[pixel_index + width*3 + 1]) / 2; // average of neighboring red pixels
+            }
+
+            // Store the result in the output image (RGB)
+            output_image[pixel_index] = r;  // Red channel
+            output_image[pixel_index + 1] = g;  // Green channel
+            output_image[pixel_index + 2] = b;  // Blue channel
+        }
+    }
+}
+
+void demosaic_RGGB_8bit(uint8_t *raw_data, uint8_t *output_image, int width, int height) {
+    int i, j;
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            int pixel_index = i * width*3 + j*3;
+            int r, g, b;
+
+            if (i % 2 == 0 && j % 2 == 0) {
+                // Red pixel
+                r = raw_data[pixel_index];
+                g = (raw_data[pixel_index + 1] + raw_data[pixel_index + width*3]) / 2; // average of neighboring green pixels
+                b = (raw_data[pixel_index + width*3 + 1] + raw_data[pixel_index + width*3 - 1]) / 2; // average of neighboring blue pixels
+            } else if (i % 2 == 0 && j % 2 == 1) {
+                // Green pixel (even row, odd column)
+                g = raw_data[pixel_index];
+                r = (raw_data[pixel_index - 1] + raw_data[pixel_index + 1]) / 2; // average of neighboring red pixels
+                b = (raw_data[pixel_index + width*3] + raw_data[pixel_index + width*3 - 1]) / 2; // average of neighboring blue pixels
+            } else if (i % 2 == 1 && j % 2 == 0) {
+                // Green pixel (odd row, even column)
+                g = raw_data[pixel_index];
+                r = (raw_data[pixel_index - width*3] + raw_data[pixel_index + width*3]) / 2; // average of neighboring red pixels
+                b = (raw_data[pixel_index - 1] + raw_data[pixel_index + 1]) / 2; // average of neighboring blue pixels
+            } else {
+                // Blue pixel
+                b = raw_data[pixel_index];
+                g = (raw_data[pixel_index - width*3] + raw_data[pixel_index + width*3]) / 2; // average of neighboring green pixels
+                r = (raw_data[pixel_index - width*3 - 1] + raw_data[pixel_index + width*3 + 1]) / 2; // average of neighboring red pixels
+            }
+
+            // Store the result in the output image (RGB)
+            output_image[pixel_index] = r;  // Red channel
+            output_image[pixel_index + 1] = g;  // Green channel
+            output_image[pixel_index + 2] = b;  // Blue channel
+        }
+    }
+}
+
+int ConvertFITtoTIF(const wchar_t* inputPath, int outputFormat, BOOL demosaic) {
     FILE* inFile = NULL;
     FILE* outFile = NULL;
     int success = 0;
@@ -214,36 +366,6 @@ int ConvertFITtoTIF(const wchar_t* inputPath) {
         ShowError(NULL, L"Could not open input file");
         goto cleanup;
     }
-
-    // Read first header block
-    // FITHeader header;
-    // if (fread(&header, sizeof(header), 1, inFile) != 1) {
-    //     ShowError(NULL, L"Could not read FITS header");
-    //     goto cleanup;
-    // }
-
-    // Parse header cards
-    // for (int i = 0; i < 36; i++) {
-    //     const char* card = header.cards[i].card;
-        
-    //     // Debug output for each card
-    //     char debug_card[81] = {0};
-    //     strncpy_s(debug_card, sizeof(debug_card), card, 80);
-    //     printf("Card %d: '%s'\n", i, debug_card);
-
-    //     // Parse specific values
-    //     parse_card_value(card, "BITPIX", &bitpix);
-    //     parse_card_value(card, "NAXIS1", &width);
-    //     parse_card_value(card, "NAXIS2", &height);
-    //     parse_card_value(card, "NAXIS3", &channels);
-    //     parse_card_value(card, "BZERO", &bzero);
-
-    //     // Check for END card
-    //     if (strncmp(card, "END", 3) == 0) {
-    //         printf("END card found\n");
-    //         break;
-    //     }
-    // }
     
     // keep reading cards until END card is found
     int i = 0;
@@ -301,7 +423,7 @@ int ConvertFITtoTIF(const wchar_t* inputPath) {
     }
 
     // Validate BITPIX
-    if (bitpix != 8 && bitpix != 16 && bitpix != -32 && bitpix != -64) {
+    if (bitpix != 8 && bitpix != 16) {
         ShowError(NULL, L"Unsupported BITPIX value: %d", bitpix);
         goto cleanup;
     }
@@ -315,22 +437,12 @@ int ConvertFITtoTIF(const wchar_t* inputPath) {
     } else if (bitpix == 16) {
         image_data = malloc(data_size * sizeof(uint16_t));
         pixel_size = sizeof(uint16_t);
-    } else if (bitpix == -32) {
-        image_data = malloc(data_size * sizeof(float));
-        pixel_size = sizeof(float);
     } else {
         ShowError(NULL, L"Unsupported BITPIX value: %d", bitpix);
         goto cleanup;
     }
 
-    // size_t r = fread(image_data, pixel_size, data_size, inFile);
-    // if (r != data_size) {
-    //     ShowError(NULL, L"Could not read image data, got %d expected %d", r, data_size);
-    //     free(image_data);
-    //     goto cleanup;
-    // }
-
-    // read image data, one channel at a time
+    // read image data, one channel at a time due to how FIT data is stored
     void* tmpPixel = malloc(pixel_size);
     for (int i = 0; i < channels; i++) {
         for (int j = 0; j < width * height; j++) {
@@ -340,60 +452,99 @@ int ConvertFITtoTIF(const wchar_t* inputPath) {
                 ((uint8_t*)image_data)[j * channels + i] = ((uint8_t*)tmpPixel)[0];
             } else if (bitpix == 16) {
                 ((uint16_t*)image_data)[j * channels + i] = ((uint16_t*)tmpPixel)[0];
-            } else if (bitpix == -32) {
-                ((float*)image_data)[j * channels + i] = ((float*)tmpPixel)[0];
             }
         }
     }
     free(tmpPixel);
 
-    uint8_t *data_8bit = (uint8_t *)malloc(width * height * channels);
-    if (!data_8bit) {
-        ShowError(NULL, L"Could not allocate memory for image data");
-        goto cleanup;
-    }
-
-    if (bitpix == 8) {
-        memcpy(data_8bit, image_data, width * height * channels);
-    } else if (bitpix == 16) {
-        for (int i = 0; i < width * height * channels; i++) {
-            // float d = ((((uint16_t*)image_data)[i] - bzero) >> 8) / 255.0f;
-            uint8_t d = (((uint16_t*)image_data)[i]) >> 8;
-            // data_8bit[i] = (uint8_t)(d * 255);
-            data_8bit[i] = d;
+    if (outputFormat == 0) { // TIFF
+         // Create output filename (replace .FIT with .TIF)
+        wchar_t filepath_w[MAX_PATH];
+        wcscpy_s(filepath_w, MAX_PATH, inputPath);
+        wchar_t* ext = wcsrchr(filepath_w, L'.');
+        if (ext) {
+            wcscpy_s(ext, 5, L".TIF");
+            // wcscpy_s(ext, 5, L".JPG");
         }
-    } else if (bitpix == -32) {
-        ShowError(NULL, L"Unsupported BITPIX value for conversion: %d", bitpix);
-        goto cleanup;
-    } else if (bitpix == -64) { 
-        ShowError(NULL, L"Unsupported BITPIX value for conversion: %d", bitpix);
-        goto cleanup;
-    }
 
-    // Create output filename (replace .FIT with .TIF)
-    wchar_t filepath_w[MAX_PATH];
-    wcscpy_s(filepath_w, MAX_PATH, inputPath);
-    wchar_t* ext = wcsrchr(filepath_w, L'.');
-    if (ext) {
-        wcscpy_s(ext, 5, L".TIF");
-        // wcscpy_s(ext, 5, L".JPG");
-    }
+        // filepath to simple char array
+        char filepath[MAX_PATH];
+        wcstombs(filepath, filepath_w, MAX_PATH);
 
-    // filepath to simple char array
-    char filepath[MAX_PATH];
-    wcstombs(filepath, filepath_w, MAX_PATH);
+        void* image_data_demosaic = malloc(width * height * channels * sizeof(uint16_t));
+        if (demosaic) {
+            demosaic_RGGB_16bit((uint16_t*)image_data, (uint16_t*)image_data_demosaic, width, height);
+        }
 
-    // write png version
-    if (!stbi_write_jpg(filepath, width, height, channels, data_8bit, 100)) {
-        ShowError(NULL, L"Could not write image data");
-        goto cleanup;
-    }
+        // write tiff version
+        if (!write_simple_tiff(filepath, demosaic ? (uint16_t*)image_data_demosaic : (uint16_t*)image_data, bitpix, width, height, channels)) {
+            ShowError(NULL, L"Could not write tiff image data");
+            goto cleanup;
+        }
+    } else {
+        // Create output filename (replace .FIT with .TIF)
+        wchar_t filepath_w[MAX_PATH];
+        wcscpy_s(filepath_w, MAX_PATH, inputPath);
+        wchar_t* ext = wcsrchr(filepath_w, L'.');
+        if (ext) {
+            // wcscpy_s(ext, 5, L".TIF");
+            if (outputFormat == 1) {
+                wcscpy_s(ext, 5, L".JPG");
+            } else {
+                wcscpy_s(ext, 5, L".PNG");
+            }
+        }
 
-    // write tiff version
-    if (!write_simple_tiff(filepath, image_data, bitpix, width, height, channels)) {
-        ShowError(NULL, L"Could not write tiff image data");
-        goto cleanup;
+        // filepath to simple char array
+        char filepath[MAX_PATH];
+        wcstombs(filepath, filepath_w, MAX_PATH);
+
+        uint8_t *data_8bit = 0x0;
+        uint8_t *data_8bit_raw = (uint8_t *)malloc(width * height * channels);
+        uint8_t *data_8bit_demosaic = (uint8_t *)malloc(width * height * channels);
+        if (!data_8bit_raw || !data_8bit_demosaic) {
+            ShowError(NULL, L"Could not allocate memory for image data");
+            goto cleanup;
+        }
+
+        if (bitpix == 8) {
+            memcpy(data_8bit_raw, image_data, width * height * channels);
+        } else if (bitpix == 16) {
+            for (int i = 0; i < width * height * channels; i++) {
+                uint8_t d = (((uint16_t*)image_data)[i]) >> 8;
+                data_8bit_raw[i] = d;
+            }
+        } else if (bitpix == -32) {
+            ShowError(NULL, L"Unsupported BITPIX value for conversion: %d", bitpix);
+            goto cleanup;
+        } else if (bitpix == -64) { 
+            ShowError(NULL, L"Unsupported BITPIX value for conversion: %d", bitpix);
+            goto cleanup;
+        }
+
+        if (demosaic) {
+            demosaic_RGGB_8bit(data_8bit_raw, data_8bit_demosaic, width, height);
+            data_8bit = data_8bit_demosaic;
+        } else {
+            data_8bit = data_8bit_raw;
+        }
+
+        // write png version
+        // if (!stbi_write_jpg(filepath, width, height, channels, data_8bit, 100)) {
+        if (outputFormat == 2) {
+            if (!stbi_write_png(filepath, width, height, channels, data_8bit, width * channels)) {
+                ShowError(NULL, L"Could not write PNG data");
+                goto cleanup;
+            }
+        } else {
+            if (!stbi_write_jpg(filepath, width, height, channels, data_8bit, 100)) {
+                ShowError(NULL, L"Could not write JPG data");
+                goto cleanup;
+            }
+        }
     }
+    
+
 
     success = 1;
 
